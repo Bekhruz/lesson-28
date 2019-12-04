f498898dc2fa43c24978e1af763120cc133a940e
BekhruzMM <b.mirtursinov@student.inha.uz>
2019-12-04 11:44:11 +0500

    Directory.selector/directory.reducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
-import MenuItem from '../menu-item/menu-item.component';
+import MenuItem from "../menu-item/menu-item.component";
 
-import { DirectoryMenuContainer } from './directory.styles';
+import { DirectoryMenuContainer } from "./directory.styles";
 
 const Directory = ({ sections }) => (
   <DirectoryMenuContainer>
-    {sections.map(({ id, ...otherSectionProps }) => (
-      <MenuItem key={id} {...otherSectionProps} />
-    ))}
+    {sections.map(({ id, ...otherSectionProps }) => {
+      return <MenuItem key={id} {...otherSectionProps} />;
+    })}
   </DirectoryMenuContainer>
 );
 
